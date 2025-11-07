7457fbdf7161033112f25f7dfac895aff7c939be
corjqnrl <coqnrl@ewha.ac.kr>
2025-11-07 22:17:50 +0900

    feat: 숙소 상세 페이지 편의 시설 ui

--- airbnb/src/app/lodgingDetail/page.tsx
+++ airbnb/src/app/lodgingDetail/page.tsx
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import Review from '@/app/common/review';
-import HostPrice from '@/app/lodgingDetail/hostPrice';
+import Convenience from '@/app/lodgingDetail/convenience';
 
 
 export default function Page() {
-  return <HostPrice />
+  return <Convenience />
 }
