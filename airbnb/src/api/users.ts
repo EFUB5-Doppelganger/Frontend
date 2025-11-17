ff86c39e37743ad3220c815b46a474f8ab8f898e
corjqnrl <coqnrl@ewha.ac.kr>
2025-11-17 21:09:09 +0900

    feat: 로그인 api 추가

--- airbnb/src/api/users.ts
+++ airbnb/src/api/users.ts
@@ -12,9 +12,25 @@ export interface SignUpResponse {
   provider: "general";
 }
 
+export interface LoginPayload {
+  accessToken: string;
+}
+
+export interface LoginResponse {
+  success: boolean;
+  message: string;
+}
+
 // 회원가입 
 export async function signUp(data: SignUpPayload): Promise<SignUpResponse> {
   return apiClient<SignUpResponse>("/members", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
+
+export async function Login(data: LoginPayload): Promise<LoginResponse> {
+  return apiClient<LoginResponse>("/users/login/general", {
     method: "POST",
     body: JSON.stringify(data),
   });
