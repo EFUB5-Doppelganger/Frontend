7fbb39bc05e3606685050c68e61e0e2860c47926
corjqnrl <coqnrl@ewha.ac.kr>
2025-11-28 00:34:40 +0900

    feat: 숙소 관련 api 추가

---
+++ airbnb/src/api/acoommodations.ts
@@ -0,0 +1,50 @@
+import { apiClient } from "@/api/client"; 
+import { Dict } from "styled-components/dist/types";
+
+export interface AccommodationCard {
+  id: string;
+  name: string;
+  rating: number;
+  image: string;
+  location: string;
+  price: number;
+}
+
+export interface AccommodationPhoto {
+  url: string;
+  displayOrder: number;
+}
+
+export interface GetAccommodationDetail {
+  id: number;
+  name: string;
+  description: string;
+  dailyPrice: number;
+  rating: number;
+  maxGuests: number;
+  address: string;
+  amenities: string[];
+  photos: AccommodationPhoto[];
+}
+
+export interface GetAccommodationCardResponse {
+  accommodations: AccommodationCard[];
+}
+
+export async function getAccommodationCardRecent(): Promise<GetAccommodationCardResponse> {
+  return apiClient<GetAccommodationCardResponse>(
+    '/accommodations/card?sortBy=createdAt', { method: "GET" }
+  );
+}
+
+export async function getAccommodationCardRating(): Promise<GetAccommodationCardResponse> {
+  return apiClient<GetAccommodationCardResponse>(
+    '/accommodations/card?sortBy=rating', { method: "GET" }
+  );
+}
+
+export async function getAccommodationDetail(accommodationId : number): Promise<GetAccommodationDetail> {
+  return apiClient<GetAccommodationDetail>(
+    `{/accommodations/${accommodationId}}`, { method: "GET" }
+  );
+}
