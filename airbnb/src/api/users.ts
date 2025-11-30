7e137c933dda548787d529a4f19fa6b52730cfbc
corjqnrl <coqnrl@ewha.ac.kr>
2025-11-30 20:06:08 +0900

    fix: 카카오 로그인 api 경로 수정

--- airbnb/src/api/users.ts
+++ airbnb/src/api/users.ts
@@ -45,7 +45,7 @@ export async function login(data: LoginPayload): Promise<LoginResponse> {
 
 // 소셜 로그인
 export async function socialLogin(): Promise<SocialLoginResponse> {
-  return apiClient<SocialLoginResponse>("/users/login/kakao", {
+  return apiClient<SocialLoginResponse>("/oauth2/authorization/kakao", {
     method: "POST"
   });
 }
