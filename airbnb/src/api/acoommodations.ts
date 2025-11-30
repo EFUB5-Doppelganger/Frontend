9e7376c13a67d6e84fe0100769151aa990d146cf
corjqnrl <coqnrl@ewha.ac.kr>
2025-11-30 18:13:57 +0900

    fix: 숙소 예약 가능 날짜 조회 api 메서드 수정

--- airbnb/src/api/acoommodations.ts
+++ airbnb/src/api/acoommodations.ts
@@ -86,7 +86,7 @@ export async function getAccommodationCheck(
   return apiClient<GetAccommodationCheckResponse>(
     `/accommodations/${accommodationId}/check`, 
     { 
-      method: "GET",
+      method: "POST",
       body: JSON.stringify(data),
     }
   );
